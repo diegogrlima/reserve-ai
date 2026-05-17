cdff50a4d0030c2a2bed25106ba610858b33ae50
Diego Lima <diegolimareis021@gmail.com>
2026-05-17 20:23:45 -0300

    refactor: usa ResponseEntity no controller de usuários

--- src/main/java/com/github/diegogrlima/reservaai/controller/UserController.java
+++ src/main/java/com/github/diegogrlima/reservaai/controller/UserController.java
@@ -3,13 +3,17 @@ package com.github.diegogrlima.reservaai.controller;
 import com.github.diegogrlima.reservaai.dto.request.CreateUserRequestDTO;
 import com.github.diegogrlima.reservaai.dto.response.UserResponseDTO;
 import com.github.diegogrlima.reservaai.service.user.CreateUserService;
+import com.github.diegogrlima.reservaai.service.user.GetAllUsersService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,10 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final CreateUserService createUserService;
+    private final GetAllUsersService getAllUsersService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserResponseDTO create(@Valid @RequestBody CreateUserRequestDTO request) {
-        return createUserService.execute(request);
+    public ResponseEntity<UserResponseDTO> create(@Valid @RequestBody CreateUserRequestDTO request) {
+        UserResponseDTO response = createUserService.execute(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponseDTO>> getAll(Pageable pageable) {
+        Page<UserResponseDTO> response = getAllUsersService.execute(pageable);
+
+        return ResponseEntity.ok(response);
     }
 }
