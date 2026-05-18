2947525585f835aeabc76c8ada389f61a83d1017
Diego Lima <diegolimareis021@gmail.com>
2026-05-17 22:08:05 -0300

    fix: ajusta migration do Flyway

--- src/main/resources/db/migration/V7__drop_unique_constraints_from_bookings.sql
+++ src/main/resources/db/migration/V7__drop_unique_constraints_from_bookings.sql
@@ -1,4 +1,8 @@
 ALTER TABLE bookings
+    ADD INDEX idx_bookings_room_id (room_id),
+    ADD INDEX idx_bookings_user_id (user_id);
+
+ALTER TABLE bookings
     DROP INDEX uk_bookings_room;
 
 ALTER TABLE bookings
